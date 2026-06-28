0c387126fcfedc7ea4f7e4fe49384dcacb5a8aca
Manjit Kumar Mahato <manjitmahato099@gmail.com>
2026-06-28 18:45:31 +0530

    Implement file download API

--- src/main/java/com/prog/service/NotesService.java
+++ src/main/java/com/prog/service/NotesService.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.prog.dto.NotesDto;
+import com.prog.entity.FileDetails;
 
 public interface NotesService {
 
 	public Boolean saveNotes(String notes,MultipartFile file) throws Exception;
 	
 	public List<NotesDto> getAllNotes();
+
+	public byte[] downloadFile(FileDetails fileDtls) throws Exception;
+
+	public FileDetails getFileDetails(Integer id) throws Exception;
 
 }
