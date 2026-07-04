8e762c52ade60218efc8fb2919c6c171f4e05a50
Manjit Kumar Mahato <manjitmahato099@gmail.com>
2026-07-04 18:15:52 +0530

    Implement user login API

--- src/main/java/com/prog/service/UserService.java
+++ src/main/java/com/prog/service/UserService.java
@@ -1,9 +1,13 @@
 package com.prog.service;
 
+import com.prog.dto.LoginRequest;
+import com.prog.dto.LoginResponse;
 import com.prog.dto.UserDto;
 
 public interface UserService {
 
-	public Boolean register(UserDto userDto,String url)throws Exception;
+	public Boolean register(UserDto userDto, String url) throws Exception;
+
+	public LoginResponse login(LoginRequest loginRequest);
 	
 }
