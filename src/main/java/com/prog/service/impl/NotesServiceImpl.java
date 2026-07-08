59400405baaf2051f351233c9060bcae59e7fe8b
Manjit Kumar Mahato <manjitmahato099@gmail.com>
2026-07-08 15:59:20 +0530

    Implement search notes API

--- src/main/java/com/prog/service/impl/NotesServiceImpl.java
+++ src/main/java/com/prog/service/impl/NotesServiceImpl.java
@@ -43,6 +43,7 @@ import com.prog.repository.FavouriteNoteRepository;
 import com.prog.repository.FileRepository;
 import com.prog.repository.NotesRepository;
 import com.prog.service.NotesService;
+import com.prog.util.CommonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -284,6 +285,26 @@ public class NotesServiceImpl implements NotesService {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public NotesResponse getNotesByUserSearch(Integer pageNo, Integer pageSize, String keyword) {
+		Integer userId = CommonUtil.getLoggedInUser().getId();
+		Pageable pageable = PageRequest.of(pageNo, pageSize);
+		Page<Notes> pageNotes = notesRepo.searchNotes(keyword, userId, pageable);
+		List<NotesDto> notesDto = pageNotes.get().map(n->mapper.map(n, NotesDto.class)).toList();
+		
+		NotesResponse notes = NotesResponse.builder()
+				.notes(notesDto)
+				.pageNo(pageNotes.getNumber())
+				.pageSize(pageNotes.getSize())
+				.totalElements(pageNotes.getTotalElements())
+				.totalPages(pageNotes.getTotalPages())
+				.isFirst(pageNotes.isFirst())
+				.isLast(pageNotes.isLast())
+				.build();
+		
+		return notes;
 	}
 
 }
